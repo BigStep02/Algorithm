d3585747f2835458de552a8f6749c186bb659481
BigStep02 <ohseokjin1234@hanmail.net>
2025-03-02 01:31:56 +0900

    [Platinum V] Title: 찾기, Time: 208 ms, Memory: 141056 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.py
+++ 백준/Platinum/1786. 찾기/찾기.py
@@ -1,39 +1,40 @@
-def rabin_karp(): # 라빈 카프 알고리즘
-    base = 31
-    mod = 10 ** 9 + 7
-    word_hash = 0
-    pattern_hash = 0
-    base_pow = 1
-    cnt = 0
-    word_list = []
+import sys
+input = sys.stdin.readline
 
-    if len(word) < len(pattern):
-        return 0, []
+def KMP():
+    global cnt, answer
+    idx = 0
 
-    for _ in range(len(pattern) - 1):
-        base_pow = (base_pow * base) % mod
+    for i in range(len(word)): # KMP 알고리즘(문자열 매칭 실패시 불필요한 연산을 줄임)
+        while idx > 0 and word[i] != target[idx]:
+            idx = failure[idx - 1]
+        if word[i] == target[idx]:
+            if idx == len(target) - 1:
+                cnt += 1
+                answer.append(i - len(target) + 2)
+                idx = failure[idx]
+            else:
+                idx += 1
 
-    for i in range(len(pattern)):
-        pattern_hash = (pattern_hash * base + ord(pattern[i])) % mod # pattern의 초기 해시값
-        word_hash = (word_hash * base + ord(word[i])) % mod  # 단어의 초기 해시값
+def failure_func(): # 문자열 매칭에 실패했을때 얼마나 건너뛰어야 하는지 계산(최장 접미사를 구함)
+    idx = 0
+    for i in range(1, len(target)):
+        while idx > 0 and target[i] != target[idx]:
+            idx = failure[idx - 1]
+        if target[i] == target[idx]:
+            failure[i] = idx + 1
+            idx += 1
+        else :
+            failure[i] = 0
 
-    for i in range(len(word) - (len(pattern) - 1)):
-        if i > 0: # i가 0일때는 이미 초기값이 들어있음 초기에 한번 돌고 그 다음부터 해시값 갱신
-            word_hash = (word_hash - ord(word[i - 1]) * base_pow) % mod
-            word_hash = (word_hash * base + ord(word[i + len(pattern) - 1])) % mod
-            word_hash = (word_hash + mod) % mod
-        # 해시값을 설정할때 밑이 소수이기 때문에 해시 충돌이 일어날 확률이 적음 (해시값이 같다면 같은 문자열로 판단).
-        if word_hash == pattern_hash:
-            cnt += 1
-            word_list.append(i + 1)
-
-    return cnt, word_list                
-
-word = str(input())
-pattern = str(input())
+word = input().rstrip()
+target = input().rstrip()
+failure = [0 for _ in range(len(target))]
 cnt = 0
-word_list = []
-cnt, word_list = rabin_karp()
+answer = []
+
+failure_func()
+KMP()
 
 print(cnt)
-print(*word_list)
+print(*answer)
