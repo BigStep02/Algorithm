943a349057ea578e40645b44fff45f4b506edab5
BigStep02 <ohseokjin1234@hanmail.net>
2025-03-03 20:12:27 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.68 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,5 +1,4 @@
 def solution(wallpaper):
-    answer = []
     index = []
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[i])):
@@ -10,10 +9,5 @@ def solution(wallpaper):
     x1, x2 = index[0][0], index[-1][0]
     index.sort(key = lambda x : x[1])
     y1, y2 = index[0][1], index[-1][1]
-    
-    answer.append(x1)
-    answer.append(y1)
-    answer.append(x2 + 1)
-    answer.append(y2 + 1)
             
-    return answer
+    return x1, y1, x2 + 1, y2 + 1
