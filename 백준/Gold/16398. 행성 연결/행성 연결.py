5e713e141adc5c723f6ca4787f5b8cf4e38b0606
BigStep02 <ohseokjin1234@hanmail.net>
2025-03-09 21:39:35 +0900

    [Gold IV] Title: 행성 연결, Time: 2464 ms, Memory: 289572 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+
+def _union(A, B, cost):
+    global answer
+    A_parent = _find(A)
+    B_parent = _find(B)
+    if A_parent == B_parent:
+        return
+    if parent[A_parent] < parent[B_parent]:
+        answer += cost
+        parent[A_parent] += parent[B_parent]
+        parent[B_parent] = A_parent
+    else :
+        answer += cost
+        parent[B_parent] += parent[A_parent]
+        parent[A_parent] = B_parent
+        
+
+def _find(node):
+    if parent[node] < 0:
+        return node
+    parent[node] = _find(parent[node])
+    return parent[node]
+
+N = int(input())
+parent = [-1 for _ in range(N + 1)]
+edge = []
+
+for i in range(1, N + 1):
+    nums = list(map(int, input().split()))
+    for j in range(len(nums)):
+        if nums[j] == 0:
+            continue
+        edge.append([i, j + 1, nums[j]])
+
+edge.sort(key = lambda x : x[2])
+
+answer = 0
+for i in range(len(edge)):
+    _union(edge[i][0], edge[i][1], edge[i][2])
+
+print(answer)
