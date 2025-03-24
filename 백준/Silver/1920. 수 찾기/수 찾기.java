eccb51962ba651a1ba6f47a538d791a9724d3a78
BigStep02 <ohseokjin1234@hanmail.net>
2025-03-24 15:29:47 +0900

    [Silver IV] Title: 수 찾기, Time: 1060 ms, Memory: 59028 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,24 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args)throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer str = new StringTokenizer(br.readLine());
-        HashSet<Integer> hash = new HashSet<>();
-        for(int i = 0; i < n; i++){
-            hash.add(Integer.parseInt(str.nextToken()));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Hashtable<Integer, Integer> ht = new Hashtable<>();
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            ht.put(Integer.parseInt(st.nextToken()), i);
         }
-        int m = Integer.parseInt(br.readLine());
-        str = new StringTokenizer(br.readLine());
-        int result = 0;
-        for(int i = 0; i < m; i++){
-            result = Integer.parseInt(str.nextToken());
-            if(hash.contains(result)){
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++){
+            if(ht.containsKey(Integer.parseInt(st.nextToken()))){
                 System.out.println(1);
             }
             else System.out.println(0);
