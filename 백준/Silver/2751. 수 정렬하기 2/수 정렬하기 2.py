98124c30dfef6ebe2caaec6f9d5a8f724419b1c9
BigStep02 <ohseokjin1234@hanmail.net>
2025-04-08 15:25:42 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1140 ms, Memory: 236284 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+    
+    mid = len(nums) // 2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+
+    return result
+
+N = int(input())
+
+nums = [int(input()) for _ in range(N)]
+
+result = merge_sort(nums)
+
+for x in result:
+    print(x)
