4317485ff09420ed79cf2bf831c1ca380f5eec7e
BigStep02 <ohseokjin1234@hanmail.net>
2025-04-20 12:20:53 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 6, Time: 564 ms, Memory: 356424 KB -BaekjoonHub

---
+++ 백준/Silver/24484. 알고리즘 수업 － 깊이 우선 탐색 6/알고리즘 수업 － 깊이 우선 탐색 6.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 5)
+
+def DFS(node, high):
+    global cnt
+    visited[node] = cnt
+    level[node] = high
+    cnt += 1
+
+    for nextNode in graph[node]:
+        if visited[nextNode] != -1: continue
+        DFS(nextNode, high + 1)
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+visited = [-1 for _ in range(N + 1)]
+level = [0 for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, N + 1):
+    graph[i].sort(reverse=True)
+
+cnt = 1
+DFS(R, 0)
+
+answer = 0
+for i in range(1, N + 1):
+    answer += visited[i] * level[i]
+
+print(answer)
