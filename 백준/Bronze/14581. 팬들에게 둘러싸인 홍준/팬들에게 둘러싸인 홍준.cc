f56c6bb775f193112200d41ca829e98ae65c7dec
BigStep02 <ohseokjin1234@hanmail.net>
2025-08-21 23:34:51 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	string S;
+	cin >> S;
+	
+	cout << ":fan::fan::fan:" << "\n";
+	cout << ":fan::" << S << "::fan:" << "\n";
+	cout << ":fan::fan::fan:" << "\n";
+}
