5c7aadcc657d63f65ee1f3ebd1b133a3b9510393
BigStep02 <ohseokjin1234@hanmail.net>
2025-11-17 00:58:36 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int N;
+	cin >> N;
+	
+	for(int i = 0; i < N; i++){
+		int A;
+		cin >> A;
+		for(int j = 0; j < A; j++){
+			cout << "=";
+		}
+		cout << endl;
+	}
+}
