fb61d8a3d712d13087a0cf85ce035c2490bbffaa
SEOKJIN OH <ohseokjin1234@hanmail.net>
2025-12-03 15:54:18 +0900

    [Silver III] Title: 순열, Time: 164 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9742. 순열/순열.cc
+++ 백준/Silver/9742. 순열/순열.cc
@@ -23,13 +23,13 @@ void back_track(string &word, int N, int depth, int size, int &cnt){
   }
 
   for(int i = 0; i < word.size(); i++){
-    if(used[i]) continue;
-    if(i > 0 && word[i] == word[i-1] && !used[i-1]) continue;
-    permuWord.push_back(word[i]);
-    used[i] = true;
-    back_track(word, N, depth + 1, size, cnt);
-    permuWord.pop_back();
-    used[i] = false;
+    if(used[i] != true){
+      permuWord.push_back(word[i]);
+      used[i] = true;
+      back_track(word, N, depth + 1, size, cnt);
+      permuWord.pop_back();
+      used[i] = false;
+    }
   }
 }
 
@@ -38,15 +38,14 @@ int main(){
   int N;
 
   while(cin >> word >> N){
-    sort(word.begin(), word.end());
     int cnt = 0;
     used.assign(word.size(), false);
     permuWord.clear();
     found = false;
-
+      
     back_track(word, N, 0, word.size(), cnt);
       
       
-    if(!found) cout << word << " " << N << " = " << "No permutation" << endl;
-  }
-}
+    if(cnt < N) cout << word << " " << N << " = " << "No permutation" << endl;
+    }
+ }
