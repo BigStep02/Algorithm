78ba6cbac67affaab57965e3a03269ada9dd4172
SEOKJIN OH <ohseokjin1234@hanmail.net>
2026-04-16 08:37:11 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int N, M; cin >> N >> M;
+
+  for(int i = 0; i < N; i++){
+    int a, b; cin >> a >> b;
+  }
+
+  cout << "비와이";
+}
